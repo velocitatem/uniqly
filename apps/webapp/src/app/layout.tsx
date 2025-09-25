e33b55b38118b4705533e745a9e5285dec76a665
Daniel Rosel <daniel@alves.world>
2025-09-25 18:15:02 +0200

    added analytics

--- apps/webapp/src/app/layout.tsx
+++ apps/webapp/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
+
 
 const fontVariables = "font-sans";
 
@@ -21,6 +23,7 @@ export default function RootLayout({
                 <main className="min-h-screen">
                     {children}
                 </main>
+                <Analytics/>
             </body>
         </html>
     );
